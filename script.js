c2208ddae220d24252e923ed004d8e68fc1bbd3f
Georgios Vavitsas <g.vavitsas@gmail.com>
2025-12-08 19:03:22 +0100

    added cart and json - showQuestion() and answer() added

---
+++ script.js
@@ -0,0 +1,94 @@
+
+let questions = [
+    {
+        "question" : "Wer hat HTML erfunden?",
+        "answer_1" : "Robbie Williams",
+        "answer_2" : "Lady Gaga",
+        "answer_3" : "Tim Berners-Lee",
+        "answer_4" : "Justin Bieber",
+        "right_answer" : 3
+    },
+    {
+        "question" : "Was bedeutet das HTML Tag &lt;a&gt;?",
+        "answer_1" : "Text Fett",
+        "answer_2" : "Container",
+        "answer_3" : "Ein Link",
+        "answer_4" : "Kursiv",
+        "right_answer" : 3
+    },
+    {
+        "question" : "Wie bindet man eine Website in eine Website ein?",
+        "answer_1" : "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
+        "answer_2" : "&lt;iframe&gt;",
+        "answer_3" : "&lt;frame&gt;",
+        "answer_4" : "&lt;frameset&gt;",
+        "right_answer" : 2
+    },
+    {
+        "question" : "Wie stellt man Text am besten Fett dar?",
+        "answer_1" : "&lt;strong&gt;",
+        "answer_2" : "CSS nutzen",
+        "answer_3" : "&lt;bold&gt;",
+        "answer_4" : "&lt;&gt;",
+        "right_answer" : 1
+    },
+    {
+        "question" : "Welches Attribut kann man NICHT für Textarea verwenden?",
+        "answer_1" : "readonly",
+        "answer_2" : "max",
+        "answer_3" : "from",
+        "answer_4" : "spellcheck",
+        "right_answer" : 1
+    },
+    {
+        "question" : "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
+        "answer_1" : "Text Fett",
+        "answer_2" : "Container",
+        "answer_3" : "Ein Link",
+        "answer_4" : "Kursiv",
+        "right_answer" : 1
+    },
+    {
+        "question" : "Wie definiert man in Javascript eine Variable?",
+        "answer_1" : "let 100 = rate",
+        "answer_2" : "100 = let rate",
+        "answer_3" : "rate = 100",
+        "answer_4" : "let rate = 100",
+        "right_answer" : 3
+    }
+];
+
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('allQuestions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    console.log('Selected answer is', selection);
+    let selectedQuestionNumber = selection.slice(-1);
+    console.log('Selected question number is', selectedQuestionNumber);
+    
+    console.log('The right answer is', question['right_answer']);
+    
+    if(selectedQuestionNumber == question['right_answer']) {
+        console.log('Your answer is right!');
+    } else {
+        console.log('Wrong answer! Try again!');
+        
+    }
+
+}
